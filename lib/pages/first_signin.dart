e3d051a0e1c525fc8f8ca250d0469e352f5d3923
royyan <royanhusen007@gmail.com>
2024-12-16 21:17:21 +0700

    slicing header - first sigi in screen

--- lib/pages/first_signin.dart
+++ lib/pages/first_signin.dart
@@ -6,6 +6,29 @@ class FirstSignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff181A20),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70, left:40, right: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'assets/coin_icon.png',
+              width: 50,
+            ),
+            SizedBox(height: 70),
+            Text(
+              'Welcome back. \nLets make money.',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w600
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
