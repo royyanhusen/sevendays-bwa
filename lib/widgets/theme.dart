ece09eac5769844844f3529985a37459221c74bc
royyan <royanhusen007@gmail.com>
2024-12-17 13:19:26 +0700

    add theme TextStyle-empty state screen

---
+++ lib/widgets/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+// First Empty State
+Color blackColor = Color(0xff0E1954);
+
+TextStyle boldTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 16,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: Color(0xffF8F8F8),
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+// Second Empty State
+Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle descriptionTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+);
