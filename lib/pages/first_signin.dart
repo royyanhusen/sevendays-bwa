7e8e26a8899b8e3c088e63a588cb8a49aae9e4a1
royyan <royanhusen007@gmail.com>
2024-12-17 11:06:56 +0700

    add TextFormField - first sign in screen

--- lib/pages/first_signin.dart
+++ lib/pages/first_signin.dart
@@ -9,7 +9,7 @@ class FirstSignIn extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff181A20),
       body: Padding(
-        padding: const EdgeInsets.only(top: 70, left:40, right: 40),
+        padding: const EdgeInsets.only(top: 70, left: 40, right: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -21,9 +21,53 @@ class FirstSignIn extends StatelessWidget {
             Text(
               'Welcome back. \nLets make money.',
               style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.w600
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600),
+            ),
+            SizedBox(height: 70),
+            TextFormField(
+              style: GoogleFonts.openSans(color: Colors.white),
+              decoration: InputDecoration(
+                  fillColor: Color(0xff262A34),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(17),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: 'Email',
+                  hintStyle: GoogleFonts.openSans(
+                    color: Color(0xff6F7075),
+                  )),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              style: GoogleFonts.openSans(color: Colors.white),
+              decoration: InputDecoration(
+                fillColor: Color(0xff262A34),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(17),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Password',
+                hintStyle: GoogleFonts.openSans(
+                  color: Color(0xff6F7075),
+                ),
+                suffixIcon: Icon(
+                  Icons.visibility,
+                  color: Color(0xff6F7075),
+                ),
+              ),
+            ),
+            SizedBox(height: 6),
+            Container(
+              alignment: Alignment(1, 0.5),
+              child: Text(
+                'Forgot My Password',
+                style: GoogleFonts.poppins(
+                  color: Color(0xff6A6B70)
+                )
               ),
             )
           ],
