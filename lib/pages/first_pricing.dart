cd6905bc20d0bba595fc93294c91adbeab6a9140
royyan <royanhusen007@gmail.com>
2024-12-18 21:12:33 +0700

    menambahkan widget option-first pricing screen

--- lib/pages/first_pricing.dart
+++ lib/pages/first_pricing.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sevendays/widgets/theme.dart';
 
 void main() {
   runApp(const FirstPricing());
@@ -11,8 +10,8 @@ class FirstPricing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
+    Widget header() {
+      return Padding(
         padding: const EdgeInsets.only(top: 100, left: 30, right: 30),
         child: Column(
           children: [
@@ -34,7 +33,77 @@ class FirstPricing extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
+      );
+    }
+
+    Widget option() {
+      return Container(
+        width: 315,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+          border: Border.all(
+            color: Color(0xffD9DEEA),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16, left: 17, bottom: 23),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/pig_icon.png',
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 7),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Money Security',
+                      style: GoogleFonts.poppins(
+                        color: Color(0xff191919),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Support',
+                          style: GoogleFonts.poppins(
+                            color: Color(0xffA3A888),
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          '24/7',
+                          style: GoogleFonts.poppins(
+                            color: Color(0xff5343C2),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+        backgroundColor: Color(0xffFFFFFF),
+        body: Column(
+          children: [
+            header(),
+            SizedBox(height: 50),
+            option(),
+          ],
+        ));
   }
 }
