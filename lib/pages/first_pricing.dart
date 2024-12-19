213ffa9b56fb8f4cacb7efeecbb9fb86e9015b5d
royyan <royanhusen007@gmail.com>
2024-12-19 10:38:43 +0700

    basic state management-first pricing screen

--- lib/pages/first_pricing.dart
+++ lib/pages/first_pricing.dart
@@ -5,8 +5,15 @@ void main() {
   runApp(const FirstPricing());
 }
 
-class FirstPricing extends StatelessWidget {
+class FirstPricing extends StatefulWidget {
   const FirstPricing({super.key});
+
+  @override
+  State<FirstPricing> createState() => _FirstPricingState();
+}
+
+class _FirstPricingState extends State<FirstPricing> {
+  int selectedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -36,61 +43,90 @@ class FirstPricing extends StatelessWidget {
       );
     }
 
-    Widget option() {
-      return Container(
-        width: 315,
-        height: 100,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(60),
-          border: Border.all(
-            color: Color(0xffD9DEEA),
+    Widget option(
+      int index,
+      String imageUrl,
+      String title,
+      String description,
+      String subDescription,
+    ) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Container(
+          width: 315,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(60),
+            border: Border.all(
+              color: selectedIndex == index
+                  ? Color(0xff6050E7)
+                  : Color(0xffD9DEEA),
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16, left: 17, bottom: 23),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/pig_icon.png',
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 7),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Money Security',
-                      style: GoogleFonts.poppins(
-                        color: Color(0xff191919),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'Support',
-                          style: GoogleFonts.poppins(
-                            color: Color(0xffA3A888),
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 6,
-                        ),
-                        Text(
-                          '24/7',
-                          style: GoogleFonts.poppins(
-                            color: Color(0xff5343C2),
-                            fontWeight: FontWeight.w500,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+          child: Padding(
+            padding:
+                const EdgeInsets.only(top: 16, left: 17, bottom: 23, right: 28),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  imageUrl,
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: GoogleFonts.poppins(
+                          color: Color(0xff191919),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            description,
+                            style: GoogleFonts.poppins(
+                              color: Color(0xffA3A888),
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            subDescription,
+                            style: GoogleFonts.poppins(
+                              color: Color(0xff5343C2),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 41,
+                    top: 10,
+                  ),
+                  child: selectedIndex == index
+                      ? Image.asset(
+                          'assets/purple_check.png',
+                          width: 26,
+                        )
+                      : SizedBox(width: 26),
+                ),
+              ],
+            ),
           ),
         ),
       );
@@ -102,8 +138,31 @@ class FirstPricing extends StatelessWidget {
           children: [
             header(),
             SizedBox(height: 50),
-            option(),
-          ],
-        ));
+          option(
+            0,
+            'assets/pig_icon.png',
+            'Money Security',
+            'support ',
+            '24/7',
+          ),
+          SizedBox(height: 24),
+          option(
+            1,
+            'assets/paper_illustration.png',
+            'Automation',
+            'we provide ',
+            'invoice',
+          ),
+          SizedBox(height: 24),
+          option(
+            2,
+            'assets/dollar_icon.png',
+            'Balance Report',
+            'can up to ',
+            '10k',
+          ),
+        ],
+      ),
+    );
   }
 }
