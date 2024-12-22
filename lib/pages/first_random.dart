0580527818cf83680cc58e9da8324c71799d3f28
royyan <royanhusen007@gmail.com>
2024-12-22 10:55:21 +0700

    finishing-first random screen

--- lib/pages/first_random.dart
+++ lib/pages/first_random.dart
@@ -47,6 +47,142 @@ class FirstRandom extends StatelessWidget {
               place: 'The Bar',
               pricing: '\$510.000',
             ),
+            SizedBox(height: 26),
+            Container(
+              width: 315,
+              height: 161,
+              decoration: BoxDecoration(
+                color: Color(0xffFFFFFF),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Informations',
+                      style: GoogleFonts.poppins(
+                        color: Color(0xff191919),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sub total',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Delivery',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Total',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '\$600.000',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              '\$80.00',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              '\$680.000',
+                              style: GoogleFonts.poppins(
+                                color: Color(0xff191919),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 60),
+            Container(
+              width: 327,
+              height: 60,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  shadowColor: Color(0xffFFC532),
+                  elevation: 5,
+                  backgroundColor: Color(0xffFFC532),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(53),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Checkout Now',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xff2E221B),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              width: 327,
+              height: 60,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Color(0xffD9D9D9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(53),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Save to Wishlist',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xffFFFFFF),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
