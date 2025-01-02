396b24dec096852d64daf8a546d3e22ba37cb490
royyan <royanhusen007@gmail.com>
2025-01-02 15:41:24 +0700

    finishing second random screen

--- lib/pages/second_random.dart
+++ lib/pages/second_random.dart
@@ -87,8 +87,64 @@ class SecondRandom extends StatelessWidget {
                 )
               ],
             ),
-          )
+          ),
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '\$22,800',
+                    style: GoogleFonts.poppins(
+                      color: Color(0xff3F6DF6),
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    '/night',
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 220,
+                height: 60,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff3F6DF6),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(19),
+                      )),
+                  onPressed: () {},
+                  child: Text(
+                    'Continue',
+                    style: GoogleFonts.poppins(
+                      color: Color(0xffFAFAFA),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              label: '',
+            ),
+          ],
+        ),
       ),
     );
   }
